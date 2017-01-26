c93b62cdf9d674831a13b46c5d73b342aefcc3ed
Joanne Wang <ennaoj.w@gmail.com>
2017-01-26 00:43:56 -0800

    code can run now

--- fs.py
+++ fs.py
@@ -1,30 +1,47 @@
-README.TXT
+# README.TXT
 
 import os
 
 freelist = [None] * 5 # freelist = [A, A, A, None, None]
 filelist = []
+fd = None
 
 def init(fsname):
   size = 5 # TODO: get fsname's size
   #nfile = open(fsname) # open the file
-  fd = os.open(fsname, 'r')
+  fd = open(fsname)
   
 def create(filename, nbytes):
-  #tempfile = file.init(filename, nbytes)
+  tempfile = file(filename, nbytes)
   name = filename
   size = nbytes # size of file
   occupied = 0
   content = [None] * size
-  #filelist.append(tempfile) # appended file object to file list
-    
+  filelist.append(tempfile) # appended file object to file list
   # TODO: NOT ENOUGH SPACE???
   spaceneeded = nbytes # file system = 5 bytes, file A needed 3 bytes
-  for space in freelist:
-      if space is None:
-        space = filename
-        spaceneeded = spaceneeded - 1
-      if spaceneeded == 0:
-        break
-    
-init("abc.txt")
+  for pos in range(0, len(freelist)):
+    if freelist[pos] is None:
+      freelist[pos] = filename
+      spaceneeded = spaceneeded - 1
+    if spaceneeded == 0:
+      break
+  
+class file:
+  def __init__(self, filename, nbytes):
+    self.name = filename
+    self.size = nbytes # size of file
+    self.occupied = 0
+    self.content = [None] * self.size
+
+
+'''
+>>> import fs
+>>> fs.init("abc.txt")
+>>> fs.create("x", 2)
+>>> fs.freelist
+['x', 'x', None, None, None]
+>>> fs.create("y", 1)
+>>> fs.freelist
+['x', 'x', 'y', None, None]
+'''
