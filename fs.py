8a3124663db1957a263ee6c0532967959fa45c03
JohnlNguyen <John@campus-011-012.ucdavis.edu>
2017-01-26 21:04:35 -0800

    read write close and open functions

--- fs.py
+++ fs.py
@@ -1,24 +1,34 @@
 # README.TXT
 
 
+import io
 import os
+from file import File
+
 SystemSize = 0
 freeList = []
-fileList = []
+fileList = {}
+currPath = ""
+systemName = ""
+
 
 def init(fsname):
-    fd = open(fsname, "w")
-    fd.write("a" * 5) #Change 5 to be what we want the size of the system to be
+    fd = io.open(fsname, 'wb')
+    fd.write("a" * 5)  # Change 5 to be what we want the size of the system to be
     fd.close()
     pwd = os.getcwd() + "/" + str(fsname)
     global SystemSize
     global freeList
+    global systemName
+    systemName = fsname
     SystemSize = os.path.getsize(pwd)
     freeList = [None] * SystemSize
 
+
 def create(filename, nbytes):
-    new = File(filename, nbytes)
-    fileList.append(new)  # appended file object to file list
+    path = currPath + "/" + str(filename)
+    newFile = File(filename, nbytes, path)
+    fileList[path] = newFile  # appended file object to file lisMe
     global SystemSize
     if (nbytes > SystemSize):
         raise Exception('No More Space')
@@ -31,15 +41,76 @@ def create(filename, nbytes):
             SystemSize -= 1
 
 
+def open(filename, mode):  # example: filename is a
+    pathToFile = currPath + "/" + filename
+    if pathToFile not in fileList:
+        raise Exception("File does not exist.")
+    if mode not in ['r', 'w']:
+        raise Exception("Invalid Mode.")
+    fileToOpen = fileList[pathToFile]  # assign File object at pathToFile
+    fileToOpen.open = True
+    if mode == "r":
+        fileToOpen.read = True
+    if mode == "w":
+        fileToOpen.write = True
+    return pathToFile
 
-class File:
-    def __init__(self, filename, size):
-        self.name = filename
-        self.size = size  # size of file
-        self.occupied = 0
-        self.content = [None] * self.size
+
+def close(fd):
+    global SystemSize
+    global freeList
+    if fd not in fileList:
+        raise Exception("No such file descriptor.")
+    fileToClose = fileList[fd]
+    fileToClose.open = False
+    fileToClose.read = False
+    fileToClose.write = False
+
+def write(fd, writebuf):
+    global SystemSize
+    global freeList
+    if fd not in fileList:
+        raise Exception("No such file descriptor.")
+    F = fileList[fd]
+    if (F.size - F.occupied) < len(writebuf):
+        raise Exception("File not big enough")
+    if F.read is True:
+        raise Exception("File is read-only")
+    if F.open is False:
+        raise Exception("File is not open")
+    F.writeToFile(writebuf)
+    f = io.open(systemName, "wb")
+    f.write(writebuf)
+    f.close()
+
+
+def read(fd, nbytes):
+    global SystemSize
+    global freeList
+    if fd not in fileList:
+        raise Exception("No such file descriptor.")
+    file = fileList[fd]  # assign File object at pathToFile
+    bytesToRead = nbytes
+    tempString = ""
+    if file.read is not True:
+        raise Exception("Error: No permission to read this file!")
+    if nbytes > file.size:  # trying to read more bytes than size of file
+        raise Exception("Error: trying to read more bytes than length of file.")
+
+    for word in file.content:
+        tempString += word
+        bytesToRead -= 1
+        file.position += 1
+        if bytesToRead == 0:
+            break
+
+    return tempString
+
 
 init("abc.txt")
-create("x",3)
-create("y",1)
-print freeList
+create("x", 3)
+create("y", 1)
+open("x", "w")
+write("/x", "r")
+open("x", "r")
+close("/x")
