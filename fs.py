afdebccbf22afb22b29e75a483202c4b9ea1e0cd
JohnlNguyen <John@Johns-MacBook-Pro.local>
2017-01-26 16:11:32 -0800

    No More Space Exception

--- fs.py
+++ fs.py
@@ -1,47 +1,44 @@
 # README.TXT
 
-import os
 
-freelist = [None] * 5 # freelist = [A, A, A, None, None]
-filelist = []
+import os
+SystemSize = 5
+freeList = [None]*SystemSize  # freelist = [A, A, A, None, None]
+fileList = []
 fd = None
 
+
+
 def init(fsname):
-  size = 5 # TODO: get fsname's size
-  #nfile = open(fsname) # open the file
-  fd = open(fsname)
-  
+    pwd = os.getcwd() + "/" + str(fsname)
+    size = os.path.getsize(pwd)
+    print size
+    fd = open(fsname)
+
 def create(filename, nbytes):
-  tempfile = file(filename, nbytes)
-  name = filename
-  size = nbytes # size of file
-  occupied = 0
-  content = [None] * size
-  filelist.append(tempfile) # appended file object to file list
-  # TODO: NOT ENOUGH SPACE???
-  spaceneeded = nbytes # file system = 5 bytes, file A needed 3 bytes
-  for pos in range(0, len(freelist)):
-    if freelist[pos] is None:
-      freelist[pos] = filename
-      spaceneeded = spaceneeded - 1
-    if spaceneeded == 0:
-      break
-  
-class file:
-  def __init__(self, filename, nbytes):
-    self.name = filename
-    self.size = nbytes # size of file
-    self.occupied = 0
-    self.content = [None] * self.size
+    new = File(filename, nbytes)
+    fileList.append(new)  # appended file object to file list
+    global SystemSize
+    if (nbytes > SystemSize):
+        raise Exception('No More Space')
+    for pos in range(len(freeList)):
+        if nbytes == 0:
+            break
+        if freeList[pos] is None and (nbytes <= SystemSize):
+            freeList[pos] = filename
+            nbytes -= 1
+            SystemSize -= 1
 
 
-'''
->>> import fs
->>> fs.init("abc.txt")
->>> fs.create("x", 2)
->>> fs.freelist
-['x', 'x', None, None, None]
->>> fs.create("y", 1)
->>> fs.freelist
-['x', 'x', 'y', None, None]
-'''
+
+class File:
+    def __init__(self, filename, size):
+        self.name = filename
+        self.size = size  # size of file
+        self.occupied = 0
+        self.content = [None] * self.size
+
+init("abc.txt")
+create("x",3)
+create("y",1)
+print freeList
